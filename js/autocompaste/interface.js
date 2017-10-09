f8dc977958bd0c2a29c7382a6ee7f2983f54ff58
Ash <mdashmawi3001@gmail.com>
2017-10-09 19:38:46 +0800

    data1 and data2 texts changed to artile 1 and article 2. made some changes in interface.js for fetchTextComplete

--- js/autocompaste/interface.js
+++ js/autocompaste/interface.js
@@ -113,13 +113,16 @@ AutoComPaste.Interface = (function () {
                 for (var text_title in privates.texts) {
                     var titleArr = text_title.split(".");
                     var dataNum = parseInt(titleArr[0]);
-                    if (privates.texts.hasOwnProperty(text_title) && numWindows != 1) {
-                        console.log("Interface._fetchTextComplete: Creating window for text \"" + text_title + "\"");
-                        iface._createWindowForText(text_title);
-                    } else if (dataNum == textNum) {
-                        console.log("Interface._fetchTextComplete: Creating window for text \"" + text_title + "\"");
-                        iface._createWindowForText(text_title);
+                    if (privates.texts.hasOwnProperty(text_title)) {
+                        if (numWindows != 1) {
+                            console.log("Interface._fetchTextComplete: Creating window for text \"" + text_title + "\"");
+                            iface._createWindowForText(text_title);
+                        }
                     }
+                    /*else if (dataNum == textNum) {
+                                           console.log("Interface._fetchTextComplete: Creating window for text \"" + text_title + "\"");
+                                           iface._createWindowForText(text_title);
+                                       }*/
                 }
 
                 // Create a text editor window.
